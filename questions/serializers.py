d662616d31a64052afca6b4a943a53b2b1fd4fbc
AsiaWi <joannawitek@icloud.com>
2023-12-21 01:34:18 +0000

    Add replies to question table

--- questions/serializers.py
+++ questions/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Question
 from django.contrib.humanize.templatetags.humanize import naturaltime
-# from replies.serializers import ReplySerializer
+from replies.serializers import ReplySerializer
 
 class QuestionSerializer(serializers.ModelSerializer):
     '''
@@ -14,6 +14,18 @@ class QuestionSerializer(serializers.ModelSerializer):
     updated_at = serializers.SerializerMethodField()
     asked_by_profile_user = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.profile_image.url")
+    replies_count = serializers.SerializerMethodField()
+    replies = serializers.SerializerMethodField()
+    
+    def get_replies_count(self, obj):
+        return obj.reply_set.count()
+
+    def get_replies(self, obj):
+        request = self.context.get('request')
+        if request:
+            replies = obj.reply_set.all()
+            return ReplySerializer(replies, many=True, context={'request': request}).data
+        return None
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -28,7 +40,6 @@ class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = '__all__'
-
 
 class QuestionDetailsSerializer(QuestionSerializer):
     '''
